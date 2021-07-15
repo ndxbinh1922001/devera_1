aef26608b012d74ad1763241ca599ec4a5be9277
= <=>
2021-07-15 13:14:10 +0700

    by xuan binh

--- lib/main.dart
+++ lib/main.dart
@@ -32,44 +32,47 @@ class MyApp1 extends StatelessWidget {
 
 class MyApp2 extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Material(
-       child: Expanded(
-         child: Column(
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body:Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(flex: 4,
+                      child: Container(
+                    child: Image.asset('images/walk_through_1_1.png'),padding: EdgeInsets.only(top: 50),)
+                  ),
+                  Expanded(flex: 1,child: Container(
+                    child: Image.asset('images/Welcome.png')),
 
-           children: [
-             Container(
-               child : Image.asset('images/walk_through_1_1.png'),
-               padding: EdgeInsets.only(left: 35,top: 50,right: 35,bottom: 20),
-             ),
-
-
-             Container(
-                 child:Image.asset('images/Welcome.png'),
-             ),
-
-             Container(
-                 child: Image.asset('images/Whats_going_to_happe.png'),
-               padding: EdgeInsets.only(top: 9),
-             ),
-             Container(
-
-                 child: Stack(
-                   children: [
-                     Container(child: Image.asset('images/Group.png')),
-                     TextButton(
-                           onPressed: (){}, child: Text('Get Started'),
-                       ),
-                   ],
-                   alignment: Alignment(0.0,0.0),
-                 ),
-                height: 260,
-               )
-           ],
-         ),
-
-       )
-
-    );
+                  ),
+                  Expanded(flex: 1,child: Container(child: Image.asset('images/Whats_going_to_happe.png')),
+                  ),
+                  Expanded(flex: 4,child: Container(
+                    child: Stack(
+                      children: [
+                        Container(
+                          child: Image.asset('images/Group.png',width: 500,),
+                        ),
+                        Container(
+                          child: Center(
+                            child:
+                            TextButton(
+                                onPressed: (){}, child: Stack(children:[
+                                  Center(child: Image.asset('images/Rectangle_2.png')),
+                                  Center(child: Image.asset('images/Get_Started.png'))
+                            ]
+                            )),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  )
+                ],
+              ),
+            ),
+        );
   }
 }
 
